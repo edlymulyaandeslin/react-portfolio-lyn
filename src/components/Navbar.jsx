28d9931daf9bf5e4708eb84e4d2f6be16a3507ae
edly mulya andeslin <edlmulyaandeslin@gmail.com>
2024-11-10 17:30:52 +0700

    [modify]: title navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,12 @@ import {
   FaLinkedin,
   FaSquareXTwitter,
 } from "react-icons/fa6";
-import logo from "../assets/kevinRushLogo.png";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="" />
+        <p className="text-3xl font-semibold">EdMA</p>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
